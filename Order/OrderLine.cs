a9b622567c5bc41a4d693c3e11126f7ab7d0f010
Mari-Liis <Mari-Liis@DESKTOP-62CRF65>
2017-03-02 11:14:00 +0200

    muutin

--- Order/OrderLine.cs
+++ Order/OrderLine.cs
@@ -13,20 +13,21 @@ namespace Order
         public int NumberOrdered { get; set; }
         public int UnitPrice { get; set; }
 
-        //getOrderLineIdentifier() - Returns the OrderLineIdentifier—this is the unique identifier for the OrderLine
-        //incrementNumberOrdered - Increments the number of ProductInstance recorded by the OrderLine
-        //getNumberOrdered() - Returns the number of ProductInstances recorded by the OrderLine
-        //decrementNumberOrdered - Decrements the number of ProductInstances recorded by the OrderLine
+        //klass olemas getOrderLineIdentifier() - Returns the OrderLineIdentifier—this is the unique identifier for the OrderLine
+        //klassi pole vaja incrementNumberOrdered - Increments the number of ProductInstance recorded by the OrderLine
+        //klassi ei ole vaja  getNumberOrdered() - Returns the number of ProductInstances recorded by the OrderLine
+        //klassi ei ole vaja decrementNumberOrdered - Decrements the number of ProductInstances recorded by the OrderLine
+        /*olemas */
         public void addDeliveryReceiver(DeliveryReceiver line)
         {
             
         }
-        //addDeliveryReceiver - Adds a DeliveryReceiver to the OrderLine
-        //getDeliveryReceiver() - Returns the DeliveryReceiver for this OrderLine
-        //removeDeliveryReceiver - Removes the DeliveryReceiver from the OrderLine
-        //addTax(...) Adds tax (represented by a TaxOnLine) to the OrderLine
-        //getTaxes() - Returns all the TaxOnLines for this OrderLine
-        //removeTax(...) Removes a TaxOnLine from the OrderLine
+        //mariliis addDeliveryReceiver - Adds a DeliveryReceiver to the OrderLine
+        //mariliis getDeliveryReceiver() - Returns the DeliveryReceiver for this OrderLine
+        // ei tea removeDeliveryReceiver - Removes the DeliveryReceiver from the OrderLine
+        // Merilyn addTax(...) Adds tax (represented by a TaxOnLine) to the OrderLine
+        //Merilyn getTaxes() - Returns all the TaxOnLines for this OrderLine
+        // ei tea removeTax(...) Removes a TaxOnLine from the OrderLine
         public void addChargeLine(ChargeLine line)
         {
             ChargeLines.Instances.Add(line);
@@ -39,8 +40,8 @@ namespace Order
         } //Returns all the ChargeLines associated with this OrderLine
 
         
-        //removeChargeLine(...) Removes a ChargeLine from the OrderLine
-        //clone() Makes a copy of the OrderLine and any associated objects that can be used to create an amended OrderLine
+        // teeme ara removeChargeLine(...) Removes a ChargeLine from the OrderLine
+        //ei tea clone() Makes a copy of the OrderLine and any associated objects that can be used to create an amended OrderLine
 
 
 
